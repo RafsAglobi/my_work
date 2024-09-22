07b90a94ba2127aa2c670ed246432d4975f046bc
RafsAglobi <agabekov.r777@gmail.com>
2024-09-22 21:15:59 +0300

    Add files via upload

---
+++ module9.py
@@ -0,0 +1,9 @@
+def apply_all_func(int_list, *functions):
+    reuslts = {}
+    for function in functions:
+        reuslts.update({function.__name__: function(int_list)})
+    return reuslts
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
