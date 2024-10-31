8457ad8a7031c8748c0ee7f5f4786404b5e3119a
RafsAglobi <agabekov.r777@gmail.com>
2024-11-01 00:32:48 +0300

    module1_4.py

--- module1_4.py
+++ module1_4.py
@@ -1,10 +1,7 @@
-#my_string = input("Введите ваше имя:")
-#current_year = 2024
-#date_of_birth = int(input("В каком году вы родились"))
-#age = current_year - int(date_of_birth)
-#print("Здравствуйте," , my_string)
-#print("В этом году Вам", age, "лет")
-print("привет,строка в нижнем регистре".upper().lower())
-name = "my_string"
-print(name[0])
-print(name[-1])
+my_string = input('Введите ФИО: ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ' , ''))
+print(my_string[0])
+print(my_string[-1])
