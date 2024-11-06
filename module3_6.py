5a8443d36e67fb38e9995bdc1f6f9ab0d90e392c
RafsAglobi <agabekov.r777@gmail.com>
2024-11-06 12:07:09 +0300

    Update module3_6.py

--- module3_6.py
+++ module3_6.py
@@ -1,32 +1,32 @@
 data_structure = [[1, 2, 3],{'a':4, 'b':5},(6,{'cube':7,'drum':8}),"Hello",((),[{(2,'Urban',('Urban2',35))}])]
 #print(len(data_structure))
 # for i in data_structure:
-# print(type(i))
+#     print(type(i))
 def calculate_structure_sum(data_structure):
- summa = 0
- if isinstance(data_structure, list) or isinstance(data_structure, tuple) or isinstance(data_structure,set):
- for i in data_structure:
- summa += calculate_structure_sum(i)
- elif isinstance(data_structure, str):
- summa += len(data_structure)
- elif isinstance(data_structure, int) or isinstance(data_structure, float):
- summa += data_structure
- elif isinstance(data_structure, dict):
- for key, value in data_structure.items():
- summa += calculate_structure_sum(key)
- summa += calculate_structure_sum(value)
- return summa
+    summa = 0
+    if isinstance(data_structure, list) or isinstance(data_structure, tuple) or isinstance(data_structure,set):
+        for i in data_structure:
+            summa += calculate_structure_sum(i)
+    elif isinstance(data_structure, str):
+            summa += len(data_structure)
+    elif isinstance(data_structure, int) or isinstance(data_structure, float):
+            summa += data_structure
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            summa += calculate_structure_sum(key)
+            summa += calculate_structure_sum(value)
+    return summa
 print(calculate_structure_sum(data_structure))
 
 
 # print(len(data_structure))
 # for i in data_structure:
-# #print(type(i))
-# def index():
-# new_list = list(i)
-# return new_list
-# print(index())
-# print(type(i))
+#     #print(type(i))
+#     def index():
+#         new_list = list(i)
+#         return new_list
+#     print(index())
+#     print(type(i))
 
 
 
