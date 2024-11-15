baa814b9e01d21551918e8d493e65a7f884c12bc
RafsAglobi <agabekov.r777@gmail.com>
2024-11-15 15:48:24 +0300

    Update module1_5.py

--- module1_5.py
+++ module1_5.py
@@ -1,10 +1,9 @@
-immutable_var = 1, 2, 'a', 'b'
-print( immutable_var )
-immutable_var = 1, 2, True, "Stirng"
-immutable_var_2 = (1, 2, 'a' ,'b')
-immutable_var_3 = tuple([1,2,'a','b'])
+immutable_var = 13, 'banana', 100, 'apple'
 print(immutable_var)
-print(immutable_var_2)
-print(immutable_var_3)
-mutable_list = [1, 2, 'a', 'b', 'Modified']
+#immutable_var [0] = 300
+#print(immutable_var)
+#нельзя изменить значения элементов кортежа!
+mutable_list = [13, 'banana', 100, 'apple']
+mutable_list [0] = 15
+mutable_list [3] = 'peach'
 print(mutable_list)
